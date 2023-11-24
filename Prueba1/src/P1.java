b6d1d10f7c713ba5c831e6ddc1c01c681b755bfb
LeoNavFerCEU <lnavarro8494@ceuandalucia.es>
2023-11-24 18:58:59 +0100

    Changed "P1". Added a for and an addittion.

--- Prueba1/src/P1.java
+++ Prueba1/src/P1.java
@@ -4,6 +4,13 @@ public class P1 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hola");
+		
+		int suma = 0;
+		for (int cont = 0; cont < 10; cont++) {
+			cont ++;
+			suma += cont;
+		}
+		System.out.println(suma);
 
 	}
 
